0e13a4ce595ede05c19fd936724a2f039d936227
bmariem <mariem.borchani@gmail.com>
2022-02-12 11:59:27 +0100

    fix route offer/:id

--- routes/offers.js
+++ routes/offers.js
@@ -8,6 +8,55 @@ const Offer = require("../models/Offer");
 // import du middleware isAuthenticated
 const isAuthenticated = require("../middleware/isAuthenticated");
 
+// Route that allows us to retrieve a list of announcements, based on filters
+// If no filter is sent, this route will return all announcements
+router.get("/offers", async (req, res) => {
+  try {
+    const filters = {
+      limit: 3,
+      title: req.query.title ? req.query.title : "",
+      priceMin: req.query.priceMin ? Number(req.query.priceMin) : 0,
+      priceMax: req.query.priceMax ? Number(req.query.priceMax) : 100000,
+      sort: req.query.sort ? req.query.sort : "",
+      page: req.query.page ? Number(req.query.page) : 0,
+    };
+
+    const offers = await Offer.find({
+      product_price: { $gte: filters.priceMin, $lte: filters.priceMax },
+      product_name: new RegExp(filters.title, "i"),
+    }) // selectionner dans le BDD tous les documents dont le prix seront entre min et max renseigné et dont le nom est le titre rensigne
+      .sort({ product_price: filters.sort.replace("price-", "") }) // trier le resulat par ordre de prix croissant ou decroissant
+      .limit(filters.limit) // limiter le resulat a 3 document par page
+      .skip(filters.limit * filters.page) // skip a chaque page le nombre de document deja affiché dans la/les page precedents
+      .select("product_name product_price"); // afficher le resulat par le nom et le prix
+
+    const allExistingOffers = await Offer.countDocuments(filters);
+    res.status(200).json({ count: allExistingOffers, offers: offers });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// get the information of an offer according to its id
+router.get("/offer/:id", async (req, res) => {
+  try {
+    const offer = await Offer.findById(req.params.id).populate({
+      path: "owner",
+      select: "account.username account.phone account.avatar",
+    });
+
+    if (offer == null) {
+      res.status(400).json({ message: "Offer does not exists" });
+    } else {
+      res.status(200).json(offer);
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+});
+
 // publish an offer
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
   // creer une nouvelle offre
@@ -158,50 +207,6 @@ router.delete("/offer/delete", isAuthenticated, async (req, res) => {
     }
   } catch (error) {
     res.status(400).json(error.message);
-  }
-});
-
-// Route that allows us to retrieve a list of announcements, based on filters
-// If no filter is sent, this route will return all announcements
-router.get("/offers", async (req, res) => {
-  try {
-    const filters = {
-      limit: 3,
-      title: req.query.title ? req.query.title : "",
-      priceMin: req.query.priceMin ? Number(req.query.priceMin) : 0,
-      priceMax: req.query.priceMax ? Number(req.query.priceMax) : 100000,
-      sort: req.query.sort ? req.query.sort : "",
-      page: req.query.page ? Number(req.query.page) : 0,
-    };
-
-    const offers = await Offer.find({
-      product_price: { $gte: filters.priceMin, $lte: filters.priceMax },
-      product_name: new RegExp(filters.title, "i"),
-    }) // selectionner dans le BDD tous les documents dont le prix seront entre min et max renseigné et dont le nom est le titre rensigne
-      .sort({ product_price: filters.sort.replace("price-", "") }) // trier le resulat par ordre de prix croissant ou decroissant
-      .limit(filters.limit) // limiter le resulat a 3 document par page
-      .skip(filters.limit * filters.page) // skip a chaque page le nombre de document deja affiché dans la/les page precedents
-      .select("product_name product_price"); // afficher le resulat par le nom et le prix
-
-    const allExistingOffers = await Offer.countDocuments(filters);
-    res.status(200).json({ count: allExistingOffers, offers: offers });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// get the information of an offer according to its id
-router.get("/offer/:id", async (req, res) => {
-  try {
-    const offer = await Offer.findById(req.params.id).populate({
-      path: "owner",
-      select: "account.username account.phone account.avatar",
-    });
-    res.json(offer);
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ message: error.message });
   }
 });
 
